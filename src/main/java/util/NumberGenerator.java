f0f7b8d1c9b3c7b4f46ec8b37566b82bcea67f09
projectnumber03 <abarakus@bk.ru>
2019-03-10 16:15:16 +0300

    Убраны неудобные буквы

--- src/main/java/util/NumberGenerator.java
+++ src/main/java/util/NumberGenerator.java
@@ -3,7 +3,7 @@ package util;
 import java.util.Random;
 
 public class NumberGenerator {
-    private final static String letters = "АБВГДЕЖИКЛМНОПРСТУФХЦЧШЩЭЮЯ";
+    private final static String letters = "АБВГДЕЖИКЛМНПРСТУФХЦЧШЩЭЮЯ";
     private final static String numbers = "0123456789";
     private final static int N1 = letters.length();
     private final static int N2 = numbers.length();
